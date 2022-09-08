212b92bb5bb0f71afe4ed997740207a8bfc710b1
davidmalina <davidmalina@localhost>
2022-09-08 12:36:35 -0500

    Done! Studio 1

---
+++ src/studio1/Average.java
@@ -0,0 +1,18 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in); 
+		System.out.println("Enter your first integer: ");
+		double n1 = in.nextDouble();
+		System.out.println("Enter your second integer: ");
+		double n2 = in.nextDouble();
+		double n3 = ((n1 + n2)/2);
+		System.out.println("Your average is: " + n3);
+	}
+
+}
---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,17 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+		
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the year: ");
+		int x = in.nextInt();
+		boolean Leap = (x % 4 == 0) && (x % 100 > 0) || (x % 400 == 0);
+		System.out.print(Leap);
+		// TODO Auto-generated method stub
+
+	}
+
+}
